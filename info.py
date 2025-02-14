454665bebf87e35315b67780f176f9fb92a6e71b
coll12345 <152050891+coll12345@users.noreply.github.com>
2025-02-14 12:18:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dileepkhdileepkh88:<db_password>@cluster0.7yvdk.mongodb.net/")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dileepkhdileepkh88:<N7EdU0UgklaxO7Jj>@cluster0.7yvdk.mongodb.net/")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
